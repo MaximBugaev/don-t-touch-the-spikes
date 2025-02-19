8957221f3acb328ad1c0a9f30bf6f5480d586a26
Max Bugaev <jamesst002211@gmail.com>
2025-02-19 23:21:00 +0400

    main: вынесен конфиг для работы с сервером

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC6uXEyfnxTbg4cYtr9fSodKPyaG7PErS0",
+    authDomain: "donttouchthespikes-e4590.firebaseapp.com",
+    projectId: "donttouchthespikes-e4590",
+    storageBucket: "donttouchthespikes-e4590.firebasestorage.app",
+    messagingSenderId: "673251901973",
+    appId: "1:673251901973:web:20d44e28de634e06af841c",
+    measurementId: "G-78X0HLL6GJ"
+};  
